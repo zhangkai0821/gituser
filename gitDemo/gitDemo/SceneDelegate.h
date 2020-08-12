5b7b3567343364f8b0bb217bf0b33b8487360ddb
zhangkai <>
2020-08-12 17:23:26 +0800

    初始化项目

---
+++ gitDemo/gitDemo/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  gitDemo
+//
+//  Created by zhangkai on 2020/8/12.
+//  Copyright © 2020 zhangkai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
