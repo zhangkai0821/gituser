668e970648a514538175de3e3cecb93fedce04dc
zhangkai <>
2020-08-13 16:48:25 +0800

    增加代码

--- gitDemo/gitDemo/ViewController.m
+++ gitDemo/gitDemo/ViewController.m
@@ -7,8 +7,8 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
+#import <WebKit/WebKit.h>
+@interface ViewController ()<WKUIDelegate,WKNavigationDelegate>
 
 @end
 
@@ -17,6 +17,28 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    
+    WKWebView *webView = [[WKWebView alloc] initWithFrame:self.view.bounds];
+    //    [webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.baidu.com"]]];
+        [self.view addSubview:webView];
+        webView.UIDelegate = self;
+        webView.navigationDelegate = self;
+    
+
+    NSString *fileStr = [[NSBundle mainBundle] pathForResource:@"hunan" ofType:@"txt"];
+    NSString *str = [NSString stringWithContentsOfFile:fileStr encoding:NSUTF8StringEncoding error:nil];
+    
+    
+    
+    NSString *strHTML = str;
+        [webView loadHTMLString:strHTML baseURL:nil];
+    
+    
+    
+    
+    
+   
 }
 
 
