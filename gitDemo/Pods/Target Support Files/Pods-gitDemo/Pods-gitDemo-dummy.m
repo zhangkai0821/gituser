e9d9c51d1d10c98c813443faa59063308d388585
zhangkai <>
2020-08-17 11:32:23 +0800

    增加swift项目

--- gitDemo/Pods/Target Support Files/Pods-gitDemo/Pods-gitDemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_gitDemo : NSObject
-@end
-@implementation PodsDummy_Pods_gitDemo
-@end
---
+++ swiftDemo/swiftDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swiftDemo
+//
+//  Created by zhangkai on 2020/8/17.
+//  Copyright © 2020 zhangkai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
